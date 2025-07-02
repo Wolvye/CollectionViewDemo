fe40bd7f104033abf0549a2be2384d78ed149f09
Wolvye <alainsoltau@googlemail.com>
2025-07-02 22:14:41 +0200

    Update

--- CollectionViewDemo/App.xaml.cs
+++ CollectionViewDemo/App.xaml.cs
@@ -11,7 +11,7 @@ namespace CollectionViewDemo
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new DataView());
+            return new Window(new LayoutsPage());
         }
     }
 }
