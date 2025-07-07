78ecc815921899aff3f963851420450736ea0518
Wolvye <alainsoltau@googlemail.com>
2025-07-07 09:24:54 +0200

    Update

---
+++ CollectionViewDemo/MVVM/Models/ProductsGroup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionViewDemo.MVVM.Models
+{
+    public class ProductsGroup : List<Produkt>
+    {
+        public string Name {  get; set; }
+        public ProductsGroup(string name, List<Produkt> products) : base(products)
+        {
+            Name = name;
+        }
+    }
+}
